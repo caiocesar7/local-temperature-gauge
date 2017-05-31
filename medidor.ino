d6e1c71f028e49932b290cccd245f48c715dacd1
Caio Cesar <caiovega7@gmail.com>
2017-05-31 12:14:06 -0300

    script do medidor

---
+++ medidor.ino
@@ -0,0 +1,33 @@
+#define DHTPIN A0 
+#define DHTTYPE DHT11 
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+ 
+void loop() 
+{
+  
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else
+  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+    delay(3000);
+  }
+}
